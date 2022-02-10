e38c2a5d499c197045e1f8fe9c50de4e51a869fe
Pallat Anchaleechamaikorn <pallat.anc@cjexpress.co.th>
2022-02-10 16:40:57 +0700

    1/2- 3

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,6 +8,28 @@ import (
 )
 
 func main() {
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:8081/foobar/5", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer resp.Body.Close()
+
+	var m foobarMessage
+	if err := json.NewDecoder(resp.Body).Decode(&m); err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(m)
+}
+
+func main2() {
 	resp, err := http.Get("http://localhost:8081/foobar/5")
 	if err != nil {
 		log.Panic(err)
